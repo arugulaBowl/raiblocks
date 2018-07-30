0f416f72d3d67312c9df6c3db5ad54eb1864748d
Arugula Bowl <arugula.bowl@gmail.com>
2018-07-30 22:32:34 +0000

    Fix compiler warnings
    ../gtest/include/gtest/gtest.h: In instantiation of 'testing::AssertionResult testing::internal::CmpHelperEQ(const char*, const char*, const T1&, const T2&) [with T1 = int; T2 = long unsigned int]':
    ../gtest/include/gtest/gtest.h:1524:23:   required from 'static testing::AssertionResult testing::internal::EqHelper<true>::Compare(const char*, const char*, const T1&, const T2&, typename testing::internal::EnableIf<(! testing::internal::is_pointer<T2>::value)>::type*) [with T1 = int; T2 = long unsigned int; typename testing::internal::EnableIf<(! testing::internal::is_pointer<T2>::value)>::type = void]'
    ../rai/core_test/conflicts.cpp:12:2:   required from here
    ../gtest/include/gtest/gtest.h:1448:16: error: comparison between signed and unsigned integer expressions [-Werror=sign-compare]
       if (expected == actual) {
           ~~~~~~~~~^~~~~~~~~

--- rai/core_test/conflicts.cpp
+++ rai/core_test/conflicts.cpp
@@ -9,16 +9,16 @@ TEST (conflicts, start_stop)
 	rai::keypair key1;
 	auto send1 (std::make_shared<rai::send_block> (genesis.hash (), key1.pub, 0, rai::test_genesis_key.prv, rai::test_genesis_key.pub, 0));
 	ASSERT_EQ (rai::process_result::progress, node1.process (*send1).code);
-	ASSERT_EQ (0, node1.active.roots.size ());
+	ASSERT_EQ (0u, node1.active.roots.size ());
 	auto node_l (system.nodes[0]);
 	node1.active.start (send1);
-	ASSERT_EQ (1, node1.active.roots.size ());
+	ASSERT_EQ (1u, node1.active.roots.size ());
 	auto root1 (send1->root ());
 	auto existing1 (node1.active.roots.find (root1));
 	ASSERT_NE (node1.active.roots.end (), existing1);
 	auto votes1 (existing1->election);
 	ASSERT_NE (nullptr, votes1);
-	ASSERT_EQ (1, votes1->votes.rep_votes.size ());
+	ASSERT_EQ (1u, votes1->votes.rep_votes.size ());
 }
 
 TEST (conflicts, add_existing)
@@ -34,13 +34,13 @@ TEST (conflicts, add_existing)
 	rai::keypair key2;
 	auto send2 (std::make_shared<rai::send_block> (genesis.hash (), key2.pub, 0, rai::test_genesis_key.prv, rai::test_genesis_key.pub, 0));
 	node1.active.start (send2);
-	ASSERT_EQ (1, node1.active.roots.size ());
+	ASSERT_EQ (1u, node1.active.roots.size ());
 	auto vote1 (std::make_shared<rai::vote> (key2.pub, key2.prv, 0, send2));
 	node1.active.vote (vote1);
-	ASSERT_EQ (1, node1.active.roots.size ());
+	ASSERT_EQ (1u, node1.active.roots.size ());
 	auto votes1 (node1.active.roots.find (send2->root ())->election);
 	ASSERT_NE (nullptr, votes1);
-	ASSERT_EQ (2, votes1->votes.rep_votes.size ());
+	ASSERT_EQ (2u, votes1->votes.rep_votes.size ());
 	ASSERT_NE (votes1->votes.rep_votes.end (), votes1->votes.rep_votes.find (key2.pub));
 }
 
@@ -58,7 +58,7 @@ TEST (conflicts, add_two)
 	auto send2 (std::make_shared<rai::send_block> (send1->hash (), key2.pub, 0, rai::test_genesis_key.prv, rai::test_genesis_key.pub, 0));
 	ASSERT_EQ (rai::process_result::progress, node1.process (*send2).code);
 	node1.active.start (send2);
-	ASSERT_EQ (2, node1.active.roots.size ());
+	ASSERT_EQ (2u, node1.active.roots.size ());
 }
 
 TEST (votes, contested)
